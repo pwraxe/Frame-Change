aeddf14efa9f386c6c4af10056bc213804b85021
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2018-08-16 19:33:24 +0530

    Add files via upload

---
+++ FrameChange/FirstFrame.java
@@ -0,0 +1,34 @@
+
+package FrameChange;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class FirstFrame 
+{
+
+    public FirstFrame() 
+    {
+        JFrame f1 = new JFrame("First Frame");
+        f1.setBounds(100, 100, 400, 600);
+        f1.setLayout(null);
+        JButton b = new JButton("First");
+        b.setBounds(100, 100, 100, 40);
+        f1.add(b);
+        
+        
+        b.addActionListener((ActionEvent e) -> 
+        {
+            SecondFrame s = new SecondFrame();
+            f1.dispose();
+            
+        });
+        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f1.setVisible(true);
+    }
+    
+    
+   
+}
